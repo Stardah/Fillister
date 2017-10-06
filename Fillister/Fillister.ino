115e017029f9f47c4f1456a4f1cdabe25716bf03
Adrax <terlychnikita@gmail.com>
2017-10-06 15:36:18 +0500

    Добавьте файлы проекта.

---
+++ Fillister/Fillister.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Fillister.ino
+ Created:	06.10.2017 15:36:06
+ Author:	Adrax
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
